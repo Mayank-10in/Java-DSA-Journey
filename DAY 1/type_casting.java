20b227bec94905cd0dfadead063c3d260aa7fbad
Mayank-10in <mayankjamnadas@gmail.com>
2026-07-23 18:10:35 +0530

    DAY 1 java program

---
+++ DAY 1/type_casting.java
@@ -0,0 +1,10 @@
+// Type casting , narrowing , explicit 
+import java.util.*;
+public class type_casting{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float num = 95.55f;
+        int num1 = (int) num;
+        System.out.println(num1);
+    }
+}
---
+++ DAY 1/type_conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class type_conversion{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float number = sc.nextInt();
+        System.out.println(number);
+        
+    }
+}
+// byte -> short -> int -> float -> long -> double
